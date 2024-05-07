596337cfcf5232c42cbc5bed7345fe8c998136e3
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-05-07 19:39:24 +0400

    added module federation in host-ap

--- apps/host-app/src/app/app.routes.ts
+++ apps/host-app/src/app/app.routes.ts
@@ -1,21 +1,25 @@
-import { Route } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   // {
   //   path: '',
   //   component: NxWelcomeComponent,
+  // },
+  // {
+  //   path: '',
+  //   component: NxWelcomeComponent,
   //   pathMatch: 'full',
   // },
-    {
-        path: 'products',
-        loadComponent: () =>
-          import('@host-app/product-list').then((m) => m.ProductListComponent),
-      },
-      
-      {
-        path: 'orders',
-        loadComponent: () =>
-          import('@host-app/orders').then((m) => m.OrdersComponent),
-      },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('@host-app/product-list').then((m) => m.ProductListComponent),
+  },
+
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('@host-app/orders').then((m) => m.OrdersComponent),
+  },
 ];
